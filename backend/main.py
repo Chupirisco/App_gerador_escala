7a78150715082d7ea7ea147cda6e36d7a1b42f20
Yuri Gabriel Ferreira de Souza <yurigabrielsouza10@gmail.com>
2025-11-07 22:32:16 -0400

    feat: criado  configurado o backend :sparkles:
    vou utilizar inicialmento fastApi do python por questão de praticidade inicialmente

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from services.escala_generator import gerar_escala
+from services.escala_gerador import gerar_escala
 
 app = FastAPI()
 
 class DadosEntrada(BaseModel):
     mes: str
-    pessoas: list
-    funcoes: list
+    plantoes: list
+    individuos: dict
 
 @app.post("/gerar-escala")
 def gerar(dados: DadosEntrada):
-    resultado = gerar_escala(dados)
-    return {"escala": resultado}
+    resultado = gerar_escala(dados.dict())
+    return resultado
